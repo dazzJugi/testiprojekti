73bc9089b1d1537f378e0e22b9c3aa6ff2b4e22d
Jukka Hilvonen <jukkahilvonen@gmail.com>
2018-01-23 09:39:47 +0200

    first commit

---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+
+    public float m_Speed;
+    public int r_Speed;
+    float horizontalInput;
+    float verticalInput;
+
+    Rigidbody m_Rigidbody;
+
+    
+
+
+    // Use this for initialization
+    void Start () {
+        m_Rigidbody = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //Move the Rigidbody forwards constantly at speed you define (the blue arrow axis in Scene view)
+            m_Rigidbody.velocity = transform.forward * m_Speed;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //Move the Rigidbody backwards constantly at the speed you define (the blue arrow axis in Scene view)
+            m_Rigidbody.velocity = -transform.forward * m_Speed;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Rotate the sprite about the Y axis in the positive direction
+            transform.Rotate(new Vector3(0, r_Speed, 0) * Time.deltaTime * m_Speed, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Rotate the sprite about the Y axis in the negative direction
+            transform.Rotate(new Vector3(0, -r_Speed, 0) * Time.deltaTime * m_Speed, Space.World);
+        }
+
+        Debug.Log(transform.TransformDirection(transform.forward));
+    }
+}
---
+++ Assets/followtarget.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followtarget : MonoBehaviour {
+
+    public Transform target;
+    public float smoothTime;
+    private Vector3 velocity = Vector3.zero;
+
+    [SerializeField]
+    private Vector3 offsetPosition;
+
+    [SerializeField]
+    private Space offsetPositionSpace = Space.Self;
+
+    [SerializeField]
+    private bool lookAt = true;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+        
+	
+	// Update is called once per frame
+	void Update () {
+        Refresh();
+	}
+
+    public void Refresh()
+    {
+        if (target == null)
+        {
+            Debug.LogWarning("Missing target ref !", this);
+
+            return;
+        }
+
+        // compute position
+        if (offsetPositionSpace == Space.Self)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, target.TransformPoint(offsetPosition), ref velocity, smoothTime);
+        }
+        else
+        {
+            transform.position = transform.TransformPoint(target.forward);
+        }
+
+        // compute rotation
+        if (lookAt)
+        {
+            transform.LookAt(target);
+        }
+        else
+        {
+            transform.rotation = target.rotation;
+        }
+    }
+}
